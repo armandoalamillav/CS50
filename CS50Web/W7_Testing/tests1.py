9f44abf1c89f65b215079176164c501c8aa1ba07
armandoalamillav <armandoalamillav@gmail.com>
2024-12-14 15:09:12 -0600

    New test files

---
+++ CS50Web/W7_Testing/tests1.py
@@ -0,0 +1,35 @@
+import unittest
+
+from prime import is_prime
+
+# Define a Class that will contain all tests. It inherits from unittest.TestCase, meaning this is
+# a class that will have a lot of functions, each of which will be a test case I want to execute
+
+class Tests(unittest.TestCase):
+    def test_1(self):
+        """Check that 1 is not prime."""
+        self.assertFalse(is_prime(1))
+    
+    def test_2(self):
+        """Check that 2 is prime."""
+        self.assertTrue(is_prime(2))
+
+    def test_8(self):
+        """Check that 8 is not prime."""
+        self.assertFalse(is_prime(8))
+
+    def test_11(self):
+        """Check that 11 is prime."""
+        self.assertTrue(is_prime(11))
+
+    def test_25(self):
+        """Check that 25 is not prime."""
+        self.assertFalse(is_prime(25))
+    
+    def test_28(self):
+        """Check that 28 is not prime."""
+        self.assertFalse(is_prime(28))
+
+# if we run the program, call unittest.main which will run all unit tests
+if __name__ == "__main__":
+    unittest.main()
