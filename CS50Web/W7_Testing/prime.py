90e51cbb9b5cff6049ec4e57bcd1d72672b5f6c1
armandoalamillav <armandoalamillav@gmail.com>
2024-12-14 14:51:19 -0600

    New testing files

--- CS50Web/W7_Testing/prime.py
+++ CS50Web/W7_Testing/prime.py
@@ -1,7 +1,10 @@
 # function to validate if a number is prime
+import math
+
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, n):
+    for i in range(2, int(math.sqrt(n))):
         if n % i == 0:
             return False
+    return True
