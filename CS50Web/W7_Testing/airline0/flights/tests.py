1ffab7937208e4b8aedf01d6e7c8164ed4169bb5
armandoalamillav <armandoalamillav@gmail.com>
2024-12-16 10:12:16 -0600

    Comments were added

--- CS50Web/W7_Testing/airline0/flights/tests.py
+++ CS50Web/W7_Testing/airline0/flights/tests.py
@@ -6,6 +6,7 @@ from .models import Airport, Flight, Passenger
 # Create your tests here.
 class FlightTestCase(TestCase):
 
+    # Set up dummy data for us to test and not contaminate real data
     def setUp(self):
 
         # Create airports.
@@ -17,6 +18,7 @@ class FlightTestCase(TestCase):
         Flight.objects.create(origin=a1, destination=a1, duration=200)
         Flight.objects.create(origin=a1, destination=a2, duration=-100)
 
+    # Unit tests for different cases
     def test_departures_count(self):
         a = Airport.objects.get(code="AAA")
         self.assertEqual(a.departures.count(), 3)
@@ -42,13 +44,21 @@ class FlightTestCase(TestCase):
         f = Flight.objects.get(origin=a1, destination=a2, duration=-100)
         self.assertFalse(f.is_valid_flight())
 
+    # In addition to unit tests, we want to test entire web pages. These tests help us achieve so:
+
+    # It tests the default flights page to make sure it works correctly
     def test_index(self):
+        # Create a client that will make requests
         c = Client()
+        # Get the route for the index page and save it in response
         response = c.get("/flights/")
         print(response)
+        # Validate the status is 200, meaning OK
         self.assertEqual(response.status_code, 200)
+        # Access the context of this page and validate there are 3 flights (the ones created above)
         self.assertEqual(response.context["flights"].count(), 3)
 
+    # Validate we can access a specific flight's page
     def test_valid_flight_page(self):
         a1 = Airport.objects.get(code="AAA")
         f = Flight.objects.get(origin=a1, destination=a1)
@@ -57,6 +67,7 @@ class FlightTestCase(TestCase):
         response = c.get(f"/flights/{f.id}")
         self.assertEqual(response.status_code, 200)
 
+    # Try to get a flight that doesn't exist, get a 404 should be the expected return value
     def test_invalid_flight_page(self):
         max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
 
@@ -64,6 +75,8 @@ class FlightTestCase(TestCase):
         response = c.get(f"/flights/{max_id + 1}")
         self.assertEqual(response.status_code, 404)
 
+    # Add some passengers database
+    # The count of all passengers must be 1
     def test_flight_page_passengers(self):
         f = Flight.objects.get(pk=1)
         p = Passenger.objects.create(first="Alice", last="Adams")
